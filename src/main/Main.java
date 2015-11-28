7783ba89b873f626998f14824ef16d34a2a1488f
JoaoFagundes <joaovictordmf@gmail.com>
2015-11-28 10:33:45 -0200

    (c) Microsoft 1988

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,17 @@
 package main;
 
 import util.PokedexWrapper;
+import window.MainMenuWindow;
 import window.TeamBuilderWindowManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		new TeamBuilderWindowManager(null, new PokedexWrapper());
+		try {
+			MainMenuWindow window = new MainMenuWindow();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
